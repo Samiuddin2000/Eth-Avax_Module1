08cf3d05b9ebe77ad0df55e899a908c81c3aa23f
Samiuddin2000 <94220699+Samiuddin2000@users.noreply.github.com>
2023-05-28 13:43:54 +0530

    Create Newfile.sol

---
+++ Newfile.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract ExceptionHandling {
+    uint256 private _value;
+
+    constructor(uint256 initialValue) {
+        _value = initialValue;
+    }
+
+    function getValue() public view returns (uint256) {
+        return _value;
+    }
+
+    function setValue(uint256 newValue) public {
+        require(newValue != _value, "New value must be different");
+        _value = newValue;
+    }
+
+    function doubleValue() public {
+        uint256 doubledValue = _value * 2;
+        assert(doubledValue >= _value);
+        _value = doubledValue;
+    }
+
+    function withdraw(uint256 amount) public {
+        require(amount <= _value, "Insufficient balance");
+        _value -= amount;
+        if (_value < amount) {
+            revert("Withdrawal failed");
+        }
+    }
+}
